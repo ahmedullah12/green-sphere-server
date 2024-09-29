bd9969e80fc485a3a6fcc35bbd003b6c65aa08bb
Ahmed Ullah <gulamjoy321@gmail.com>
2024-09-29 23:24:50 +0600

    post type and model created

---
+++ src/modules/Post/post.interface.ts
@@ -0,0 +1,13 @@
+import { Types } from "mongoose";
+import { POST_CATEGORY, POST_TAG } from "./post.constant";
+
+export type TPost = {
+    title: string;
+    images: string[];
+    description: string;
+    category: (keyof typeof POST_CATEGORY)[];
+    tag: keyof typeof POST_TAG;
+    upvotes: Types.ObjectId[];
+    downvotes: Types.ObjectId[];
+    isDeleted: boolean;
+}
---
+++ src/modules/Post/post.model.ts
@@ -0,0 +1,49 @@
+import { Schema, model, Types } from "mongoose";
+import { POST_CATEGORY, POST_TAG } from "./post.constant";
+import { TPost } from "./post.interface";
+
+
+const postSchema = new Schema<TPost>({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  images: {
+    type: [String],
+    default: [],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: [String],
+    enum: Object.keys(POST_CATEGORY),
+    required: true,
+  },
+  tag: {
+    type: String,
+    enum: Object.keys(POST_TAG),
+    required: true,
+  },
+  upvotes: {
+    type: [Types.ObjectId],
+    ref: "User",
+    default: [],
+  },
+  downvotes: {
+    type: [Types.ObjectId],
+    ref: "User",
+    default: [],
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,
+});
+
+// Create the Post model
+export const Post = model("Post", postSchema);
